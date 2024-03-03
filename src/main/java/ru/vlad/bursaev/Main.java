abe2e0ffbce80eb33fd326f22d9bb0440617ef5f
Vladislav Bursaev <vlad722011@yandex.ru>
2024-03-03 12:16:57 +0700

    add

--- src/main/java/ru/vlad/bursaev/Main.java
+++ src/main/java/ru/vlad/bursaev/Main.java
@@ -2,7 +2,7 @@ package ru.vlad.bursaev;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-
+ import java.lang.String;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException{
@@ -12,7 +12,8 @@ public class Main {
         Используя Reflection API, напишите программу, которая выводит на экран все методы класса String.
          */
 
-        Class<String> stringClass = String.class;
+        Class<?> stringClass = Class.forName("java.lang.String");
+
         Method[] methods = stringClass.getDeclaredMethods();
         System.out.println("Список методов класса String: ");
         Arrays.stream(methods).forEach(System.out::println);
